a6ae876429cbc334483945e09f25007adb0a1206
wageshwari <vaishnaviwageshwari@gmail.com>
2023-05-24 14:29:28 +0530

    fix: added api to fetch price

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
 
   const fetchBitcoinPrice = async () => {
     try {
-      const result = await axios.get("");
-      var temp = prices;
-      temp.push(3000);
-      setPrice(temp);
+      const response = await axios.get(
+        "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+      );
+      const data = response?.data?.bpi?.USD?.rate;
+      setPrice([...prices, data]);
       setCurrentText("Refresh Bitcoin Price");
     } catch (er) {
       console.log(er);
@@ -19,13 +20,12 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>{prices?.[0]}</h1>
+      <h1>{prices?.[0] && `Current Bitcoin Price - ${prices[0]}`}</h1>
       {prices &&
         prices.length > 1 &&
-        prices.map((price, index) => {
-          return <p key={index}>Privous Price {price}</p>;
-        })}
-      {console.log(prices)}
+        prices
+          .slice(1)
+          .map((price, index) => <p key={index}>Privous Price - {price}</p>)}
       <button
         className={
           currentText === "Get Bitcoin Price" ? "btnBlue" : "btnPurple"
